9b28dcf06a8899d83c0abc1df5a82f4bedb4cb01
ori-des <151224246+ori-des@users.noreply.github.com>
2024-03-12 18:36:00 +0100

    Add dead function

--- game.py
+++ game.py
@@ -2,3 +2,32 @@ from sys import exit
 from time import sleep
 import random as r
 
+
+def start():
+    print("You're on the quest for the historical jesus.")
+    # sleep(1)
+    print("You are in a cave with three paths.")
+    # sleep(2)
+    print("From what path do you start? North, east or west?")
+    
+    choice = input("> ").lower()
+
+    if "west" in choice:
+        biblical_sources()
+    elif "east" in choice:
+        extrabiblical_sources()
+    elif "north" in choice:
+        jesus_not_here_room()
+    else:
+        dead("Upon too much pondering the holy spirit engulfes you and carries you to heaven. "
+             "Maybe Jesus is crucified there.")
+
+
+def biblical_sources():
+    pass
+
+def dead(reason):
+    print(reason, "You Died.")
+    exit(0)
+
+start()
